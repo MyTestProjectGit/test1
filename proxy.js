eb87933c40fa3da12b5d72a00e16b28f17d16ea0
MyTestProjectGit <oleg_2006_91@mail.ru>
2016-01-04 17:08:21 +0200

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,22 @@
+(function() {
+    var source = document.getElementsByTagName('script')[0].getAttribute('src'),
+        params = JSON.parse(decodeURIComponent(source.substring(source.indexOf('?')+1, source.lastIndexOf('&')))),
+        buffer = document.createElement('textarea'),
+        notify = document.createEvent('Event'),
+        result = {};
+    for (var param in params) {
+        var depth = params[param].split('.');
+        for (result[param] = window; depth.length; )
+            try {
+                result[param] = result[param][depth.shift()];
+            } catch(e) {
+                break;
+            }
+    }
+    buffer.style.cssText = 'display: none';
+    console.log(result);
+    buffer.value = JSON.stringify(result);
+    document.body.appendChild(buffer);
+    notify.initEvent('proxy', false, true);
+    buffer.dispatchEvent(notify);
+})();
